80f9e17af3ca68c8d15599947137b4b4cc342d97
VALON_AI <zejnulaivalon@gmail.com>
2025-06-20 02:05:38 +0200

    Initialize project structure

---
+++ valon_ai/order_block.py
@@ -0,0 +1,12 @@
+"""Order block detection module."""
+
+class OrderBlockFinder:
+    """Detect order blocks in price data."""
+
+    def __init__(self):
+        pass
+
+    def find(self, data):
+        """Return list of detected order blocks."""
+        # TODO: implement order block detection logic
+        return []
